cfdd8135acbabfb695810076257dfcb83666fc66
ambikarao <63193852+ambikarao@users.noreply.github.com>
2020-04-10 21:17:19 +0530

    lunch items counting code

---
+++ mod1_solution/app.js
@@ -0,0 +1,29 @@
+(function(){
+
+  angular.module('LunchCheck',[])
+  .controller('LunchCheckController',LunchCheckController);
+LunchCheckController.$inject = ['$scope'];
+function LunchCheckController($scope){
+  $scope.myObj = {
+   "color" : "green",
+ }
+  const str = $scope.items;
+  const words = str.split(',');
+  $scope.CheckIfTooMuch = function(words){
+    //console.log(words.length);
+    $scope.noOfItems = words.length;
+    if ($scope.noOfItems == 0) {
+      $scope.myObj = {
+       "color" : "red",
+     }
+        $scope.foodQuantity = "Please enter data first";
+    }
+    else if(($scope.noOfItems != 0) && ($scope.noOfItems <= 3)){
+      $scope.foodQuantity = "Enjoy!";
+    }
+    else {
+      $scope.foodQuantity = "Too much!";
+    }
+  }
+}
+})
