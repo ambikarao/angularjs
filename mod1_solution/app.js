19cedc42116976295a8b9bc01953ac8752278a5e
ambikarao <63193852+ambikarao@users.noreply.github.com>
2020-04-11 18:20:41 +0530

    issue5

--- mod1_solution/app.js
+++ mod1_solution/app.js
@@ -7,25 +7,30 @@ function LunchCheckController($scope){
 
   $scope.CheckIfTooMuch = function(){
     const str = $scope.items;
+    if(str === undefined){
+      $scope.noOfItems = 0;
+    }
+    else{
     const words = str.split(',');
     $scope.noOfItems = words.length;
-    if ($scope.noOfItems == 0) {
+    }
+    if ($scope.noOfItems === 0) {
       $scope.myObj = {
        "color" : "red",
      }
         $scope.foodQuantity = "Please enter data first";
     }
     else if(($scope.noOfItems != 0) && ($scope.noOfItems <= 3)){
-      $scope.foodQuantity = "Enjoy!";
       $scope.myObj = {
        "color" : "green",
      }
+      $scope.foodQuantity = "Enjoy!";
     }
     else {
-      $scope.foodQuantity = "Too much!";
       $scope.myObj = {
        "color" : "green",
      }
+      $scope.foodQuantity = "Too much!";
     }
   }
 }
